c9e5c47256622a96bc98431a10df6cdd6bf55d06
khanmahnoor <mahnoorkhan@tintash.com>
2019-06-20 16:05:43 +0500

    adding day 4's playground file

---
+++ day4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,83 @@
+//STRUCTURES AND CLASSES
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd //cinema is a different instance now
+cinema.height = 11
+
+print(hd.height)
+print(cinema.height)
+
+var vm = VideoMode()
+vm.frameRate = 10.0
+vm.interlaced = true
+vm.name = "lool"
+vm.resolution = hd
+
+var vm2 = vm
+
+print(vm.frameRate)
+vm2.frameRate = 20.0
+print(vm.frameRate)
+
+//PROPERTIES
+
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                  size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+print(square.center)
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+    
+}
